86a1e56ae3c5edb635119c9109f3a141571f672e
Amany Othman <165468609+Amany-othman@users.noreply.github.com>
2026-08-26 01:03:47 +0300

    feat: build Products page

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -1,5 +1,43 @@
-const Products = () => {
-  return <h1>Products</h1>;
-};
+import { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
+
+function Products() {
+  const [products, setProducts] = useState([]);
+  //use effect : nfz el code dh ka side effect b3d mal component ytrsm
+  // lma el page tft7 make a request enk t fetch el data
+  // []); -> shghl el effect lma el component yzhr l awl mra
+  // lakn msh kol ma el page t re-render
+  useEffect(() => {
+    const fetchProducts = async () => {
+      // req to the BE on this url to fetch the products
+      const response = await fetch("/api/products");
+      //fetch bt return response object msh el data la tol f lazem .json()
+      const data = await response.json();
+
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+  return (
+    <div>
+      <h1>Products</h1>
+
+      <div>
+        {products.map((product) => {
+          return (
+            <ProductCard
+              image={product.image}
+              name={product.name}
+              price={product.price}
+              rating={product.rating}
+              id={product._id}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
 
 export default Products;
